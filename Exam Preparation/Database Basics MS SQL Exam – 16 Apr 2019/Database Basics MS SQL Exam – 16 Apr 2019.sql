24feda3c2e511e03dcef801b62e271d9029ca428
Georgena Georgieva <georgena1991@gmail.com>
2020-04-13 13:54:55 +0300

    Update Database Basics MS SQL Exam – 16 Apr 2019.sql

--- Exam Preparation/Database Basics MS SQL Exam – 16 Apr 2019/Database Basics MS SQL Exam – 16 Apr 2019.sql
+++ Exam Preparation/Database Basics MS SQL Exam – 16 Apr 2019/Database Basics MS SQL Exam – 16 Apr 2019.sql
@@ -217,10 +217,10 @@ SELECT k.FirstName,
 	   k.Price
   FROM (
 		  SELECT p.FirstName,
-				 p.LastName, 
-				 f.Destination,
-				 t.Price,
-			     DENSE_RANK() OVER(PARTITION BY p.FirstName, p.LastName ORDER BY t.Price DESC) AS PriceRank
+		         p.LastName, 
+			 f.Destination,
+		         t.Price,
+			 DENSE_RANK() OVER(PARTITION BY p.FirstName, p.LastName ORDER BY t.Price DESC) AS PriceRank
           FROM Passengers AS p
 		  JOIN Tickets AS t ON t.PassengerId = p.Id
 		  JOIN Flights AS f ON f.Id = t.FlightId
@@ -269,9 +269,9 @@ BEGIN
 	END
 
 	DECLARE @flight INT = (SELECT f.Id
-					   FROM Flights AS f
-					   JOIN Tickets AS t ON f.Id = t.FlightId
-					   WHERE f.Destination = @destination AND f.Origin = @origin)
+			       FROM Flights AS f
+			       JOIN Tickets AS t ON f.Id = t.FlightId
+			       WHERE f.Destination = @destination AND f.Origin = @origin)
 
 	IF (@flight IS NULL)
 	BEGIN 
@@ -279,9 +279,9 @@ BEGIN
 	END 
 
 	DECLARE @price DECIMAL(15,2) = (SELECT t.Price
-									FROM Tickets AS t
-									JOIN Flights AS f ON t.FlightId = f.Id
-									WHERE f.Destination = @destination AND f.Origin = @origin)
+					FROM Tickets AS t
+					JOIN Flights AS f ON t.FlightId = f.Id
+					WHERE f.Destination = @destination AND f.Origin = @origin)
 
 	DECLARE @totalPrice DECIMAL(15,2) = @peopleCount * @price
 
@@ -314,12 +314,10 @@ GO
 --20.Deleted Planes
 --************************
 
-CREATE TABLE DeletedPlanes (
-					Id INT,
-					[Name] VARCHAR(30),
-					Seats INT, 
-					[Range] INT 
-					)
+CREATE TABLE DeletedPlanes (Id INT,
+			    [Name] VARCHAR(30),
+			    Seats INT, 
+		            [Range] INT)
 
 GO
 
